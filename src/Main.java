0045d61747a101f31b28df0b0d2ae032cfeee298
Ruslan Sharipov <ruslansharipov1992@yandex.ru>
2022-07-04 17:20:56 +0500

    Добавление файлов

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Set<Student> infoStudent = new HashSet<Student>();
+
+        while (true) {
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите информацию о студенте: (ФИО, номер группы, номер студенческого билета)");
+            System.out.println("Иванов Петр Николаевич, 1243-Б, 31231343 <enter>");
+
+            String input = scanner.nextLine();
+            String[] t = input.split(" ");
+            String a = t[0];
+            String b = t[1];
+            String c = t[2];
+
+            infoStudent.add(new Student(a, b, c));
+            System.out.println(infoStudent);
+
+
+            if (input.equals("end")) {
+                System.out.println("Список студентов: ");
+                for (Student info : infoStudent) {
+                    System.out.println(" - " + info);
+                }
+            }
+        }
+    }
+}
